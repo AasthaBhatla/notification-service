b4f7eea8bfa2c986e54545eef18d7f2593ea0168
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-05-18 21:33:47 +0530

    Update notificationQueue.js

--- queues/notificationQueue.js
+++ queues/notificationQueue.js
@@ -1,7 +1,7 @@
 const { Queue } = require('bullmq');
 const { Redis } = require('ioredis');
 
-const connection = new Redis();
+const connection = new Redis(process.env.REDIS_URL);
 
 const notificationQueue = new Queue('notificationQueue', { connection });
 
